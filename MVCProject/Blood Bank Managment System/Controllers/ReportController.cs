7ed5b42511d78a8bef1db9424680eba479f38366
ARGHA GHOSH <23-50804-1@student.aiub.edu>
2026-07-24 14:24:12 +0600

    Lab task -2 Day 2

---
+++ MVCProject/Blood Bank Managment System/Controllers/ReportController.cs
@@ -0,0 +1,60 @@
+﻿using Blood_Bank_Managment_System.EF;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blood_Bank_Managment_System.Controllers
+{
+    public class ReportController : Controller
+    {
+        BloodBankDbContext db;
+
+        public ReportController(BloodBankDbContext db)
+        {
+            this.db = db;
+        }
+
+
+        // Filter Donors by Blood Group - Block
+        public IActionResult FilterByBloodGroup(string bloodGroup)
+        {
+            var donors = string.IsNullOrEmpty(bloodGroup) 
+                ? db.Donors.ToList() 
+                : db.Donors.Where(d => d.BloodGroup == bloodGroup).ToList();
+            
+            ViewBag.SelectedBloodGroup = bloodGroup;
+            return View(donors);
+        }
+
+
+        // Sort Donors by Last Donation Date - Block
+        public IActionResult SortedByDate()
+        {
+            var donors = db.Donors.OrderBy(d => d.LastDonationDate).ToList();
+            return View(donors);
+        }
+
+
+        // Count Donations per Donor - Block
+        public IActionResult DonationCount()
+        {
+            var rs = db.Donors.Select(d => new 
+            {
+                d.FullName,
+                d.BloodGroup,
+                TotalDonations = d.Donations.Count(),
+            })
+                .ToList();
+
+            ViewBag.Title = rs;
+            return View();
+        }
+
+        // 4. Total volume collected
+        public IActionResult TotalVolume()
+        {
+            var total = db.Donations.Sum(d => d.VolumeMl);
+            ViewBag.TotalVolume = total;
+            return View();
+        }
+
+    }
+}
